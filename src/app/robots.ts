fbb53e227ae51de9c424801d4beb0a9d0511a7cd
seoweblitewebservice-gif <seoweblitewebservice@gmail.com>
2026-09-19 23:34:44 +0530

    Update canonical domain to mapbench.site

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: ["/api/"] }],
-    sitemap: "https://mapforge.tools/sitemap.xml",
+    sitemap: "https://www.mapbench.site/sitemap.xml",
   };
 }
